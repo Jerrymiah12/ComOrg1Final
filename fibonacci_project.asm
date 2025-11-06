e9afd95ea07de2e2826f9191ba66ea2c51942b6b
Jeremiah Bailey <jerrymiah12@gmail.com>
2025-11-06 16:04:50 -0500

    main

--- fibonacci_project.asm
+++ fibonacci_project.asm
@@ -3,3 +3,8 @@ prompt: .asciiz "Enter an integer N (N >= 22): "
 illegal_msg: .asciiz "Illegal Number!\n"
 fib_msg: .asciiz "Fibonacci sequence:\n"
 space: .asciiz " "
+.text
+.global main
+main:
+get_input:
+    li $v0, 4
